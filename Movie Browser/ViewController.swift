2e07adaa21250a1ba8e7181ed54a3293e71998c4
Aditya Vikram Godawat <aditya.godawat@gmail.com>
2017-11-05 20:46:22 +0530

    Initial commit

---
+++ Movie Browser/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Movie Browser
+//
+//  Created by Aditya Vikram Godawat on 11/5/17.
+//  Copyright © 2017 Aditya Vikram Godawat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
